b650613c1a3d2a293a7a407e73223f77dcc0cf6e
ZoZo-182 <zaza021803@gmail.com>
2026-02-28 00:47:43 -0500

    got rid of errors that lsp pointed out. this is my first project im doing with omarchy okay I forgot I needed to reinstall it

--- fetch_github_api.go
+++ fetch_github_api.go
@@ -4,21 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
-	"regexp"
-	"sort"
 	"strings"
-	"time"
 )
 
-
 type RepoInfo struct {
-	APIURL        string
-	Name          string
-	HTMLURL       string
-	Language      string
-	PullsURL      string
-	Stars         int
+	APIURL   string
+	Name     string
+	HTMLURL  string
+	Language string
+	PullsURL string
+	Stars    int
 }
 
 type PullRequest struct {
@@ -28,23 +23,23 @@ type PullRequest struct {
 }
 
 type RepoMetadata struct {
-	HTMLURL     string    `json:"html_url"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Language    string    `json:"language"`
-	PullsURL    string    `json:"pulls_url"`
-	Stars       int       `json:"stargazers_count"`
+	HTMLURL     string `json:"html_url"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Language    string `json:"language"`
+	PullsURL    string `json:"pulls_url"`
+	Stars       int    `json:"stargazers_count"`
 }
 
 // wtf where are am I getting a slice of repos from without any info in them??
-// I put in the info at the end so this should actually have no parameters 
+// I put in the info at the end so this should actually have no parameters
 // and instead declare an instance of RepoInfo and get rid of the loop.
 // I should turn this into a method to use on a Config struct and then use a
 // loop to access the slice of repose the cfg will hold.
 // that means i only really need two functions maybe.
 // get the list (slice) of repos with recent prs AND whose star count is 100-1k
-// this means i have to add a query about the prs and stars. store the slice in 
-// an instance of Config. 
+// this means i have to add a query about the prs and stars. store the slice in
+// an instance of Config.
 // and then use the repoDetails function on this instance to actuall see info
 // about the repos with recent prs + certain star count
 // okay no method b/c only one member, really? get the queried data, return slice,
@@ -64,7 +59,7 @@ func RepoDetails(repos []RepoInfo) ([]RepoInfo, error) {
 			Name        string `json:"name"`
 			Description string `json:"description"`
 			Language    string `json:"language"`
-			PullsURL    string `json:"pulls_url"` 
+			PullsURL    string `json:"pulls_url"`
 			Stars       int    `json:"stargazers_count"`
 		}
 		if err := json.NewDecoder(resp.Body).Decode(&repoMetaData); err != nil {
@@ -74,7 +69,7 @@ func RepoDetails(repos []RepoInfo) ([]RepoInfo, error) {
 		repos[i].Name = repoMetaData.Name
 		repos[i].HTMLURL = repoMetaData.HTMLURL
 		repos[i].Language = repoMetaData.Language
-		repos[i].PullsURL = repoMetaData.PullsURL 
+		repos[i].PullsURL = repoMetaData.PullsURL
 		repos[i].Stars = repoMetaData.Stars
 	}
 	return repos, nil
