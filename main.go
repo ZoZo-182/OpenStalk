4223b7133a6d24ef93435b2ce6b56b6124c93c39
ZoZo-182 <zaza021803@gmail.com>
2026-03-02 01:50:07 -0500

    removed trash from main.go.

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("Need at least 2 arguments. Format: ./OpenStalk [language]")
+		fmt.Println("Need at least 3 arguments. Format: ./OpenStalk [language] [within (days)]")
 	}
 
 	daysAgo, err := strconv.Atoi(os.Args[1])
@@ -28,36 +28,3 @@ func main() {
 		fmt.Printf("repo %d: %v\n", i+1, repo)
 	}
 }
-
-// split the
-
-//create topraw based off pr + star (+ language later)
-
-// Get basic repo details (name, URL, etc.)
-//	enriched, err := RepoDetails(topRaw)
-//	if err != nil {
-//		fmt.Printf("Error getting repo details: %v\n", err)
-//	}
-//
-//	// def rewrite
-//	for i, repo := range enriched {
-//		actualCount, recentIssues, err := getRepoIssueCount(repo.APIURL, 7)
-//		if err != nil {
-//			fmt.Printf("Warning: Could not get issue count for %s: %v\n", repo.Name, err)
-//			// Keep the original count if we can't get accurate count
-//		} else {
-//			enriched[i].Count = actualCount
-//			enriched[i].RecentIssues = recentIssues
-//		}
-//		// Small delay to avoid rate limiting
-//		time.Sleep(100 * time.Millisecond)
-//	}
-//
-//	// Take top 15 for UI
-//	finalCount := 15
-//	if len(filtered) > finalCount {
-//		filtered = filtered[:finalCount]
-//	}
-//
-//	launchUI(filtered)
-//}
