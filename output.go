88bbcd9bac99e66a002ca2d00d7866c1cf71f4c5
ZoZo-182 <zaza021803@gmail.com>
2026-06-27 17:09:09 -0400

    my bad i needed raw string literal

--- output.go
+++ output.go
@@ -3,22 +3,22 @@ package main
 import "fmt"
 
 func printBanner() {
-	fmt.Println("
-________                         _________ __         .__   __    
-\\_____  \\ ______   ____   ____  /   _____//  |______  |  | |  | __
- /   |   \\____ \\_/ __ \\ /    \\ \\_____  \\   __\\__  \\ |  | |  |/ /
-/    |    \\  |_> >  ___/|   |  \\/        \\|  |  / __ \\|  |_|    < 
-\\_______  /   __/ \\___  >___|  /_______  /|__| (____  /____/__|_ \\
-        \\/|__|        \\/     \\/        \\/           \\/          \\/
-				")
+	fmt.Print(`
+ ________                         _________ __         .__   __
+ \_____  \ ______   ____   ____  /   _____//  |______  |  | |  | __
+  /   |   \____ \_/ __ \ /    \ \_____  \   __\__  \ |  | |  |/ /
+ /    |    \  |_> >  ___/|   |  \/        \|  |  / __ \|  |_|    <
+ \_______  /   __/ \___  >___|  /_______  /|__| (____  /____/__|_ \
+         \/|__|        \/     \/        \/           \/          \/
+`)
 	fmt.Println("Find active projects through recent GitHub pull requests.")
 	fmt.Println()
 }
 
-func printPullRequests(prs []PullRequests) {
+func printPullRequests(prs []PullRequest) {
 	for i, pr := range prs {
 		fmt.Printf("%d. %s\n", i+1, pr.Title)
-		fmt.Printf("    %s\n", i+1, pr.HTMLURL)
+		fmt.Printf("    %s\n", pr.HTMLURL)
 		fmt.Println()
 	}
 }
