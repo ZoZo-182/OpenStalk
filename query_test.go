c6d054843965241f2c75f0bc605767c193fb6825
ZoZo-182 <zaza021803@gmail.com>
2026-06-28 00:18:19 -0400

    another query test but with language

--- query_test.go
+++ query_test.go
@@ -14,3 +14,17 @@ func TestBuildSearchQueryIncludeRequiredFilters(t *testing.T) {
 		t.Fatalf("expected %q, got %q", want, query)
 	}
 }
+
+func TestBuildSearchQueryIncludeLanguage(t *testing.T) {
+	query := buildSearchQuery(SearchOptions{
+		Language: "go",
+		MinStars: 100,
+		MaxStars: 500,
+	}, "2026-06-27")
+
+	want := "type:pr state:open created:>=2026-06-27 stars:100..500 language:go"
+
+	if query != want {
+		t.Fatalf("expected %q, got %q", want, query)
+	}
+}
